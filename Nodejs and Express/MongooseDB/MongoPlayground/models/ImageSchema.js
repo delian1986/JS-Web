cbb8f69ed540c9bab31e4a2250ed38ac6cfd1f73
delian1986 <delian1914@gmail.com>
2019-01-22 17:31:12 +0200

    exercise mongoose

---
+++ Nodejs and Express/MongooseDB/MongoPlayground/models/ImageSchema.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+let imageSchema = mongoose.Schema({
+    url: { type: mongoose.Schema.Types.String, required: true },
+    creationDate: { type: mongoose.Schema.Types.Date, default: Date.now },
+    title: { type: mongoose.Schema.Types.String, required: true },
+    description: { type: mongoose.Schema.Types.String },
+    tags: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Tag' }]
+})
+
+let Image=mongoose.model('Image',imageSchema)
+
+module.exports=Image
